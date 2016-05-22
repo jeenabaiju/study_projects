5a861984f61ef208d6665a981eb5671492bbb6f8
jeebai <jeensjohn@gmail.com>
2016-05-22 17:42:46 +0200

    changed

--- jeena_study/Mistbase/Codes_SRS/SRS_GEN_18May/check3.c
+++ jeena_study/Mistbase/Codes_SRS/SRS_GEN_18May/check3.c
@@ -363,8 +363,8 @@ static void Root_q_arg(float *root_q_arg,uint32_t u, uint32_t v, uint32_t n_ul_r
     {
         /* argument of x_q(m) according to 3GPP 36.211 5.5.1.1*/
         root_q_arg[m] = (-1 * M_PI * q * m * (m + 1)) / n_zc;
-		printf(" M_PI= %f, n_zc = %f \n",M_PI,n_zc);
-		printf(" root_q_arg[%d] = %f, n_zc = %f ,  \n",m,root_q_arg[m]);
+		printf(" M_PI= %f, n_zc = %f, q = %f, m= %d \n",M_PI,n_zc,q,m);
+		printf(" root_q_arg[%d] = %f\n",m,root_q_arg[m]);
     }
 }
 /***********************************************************************************************/
