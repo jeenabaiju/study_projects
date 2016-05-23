bc116af15d016d562cb0dd9c21519baff8e07681
jeebai <jeensjohn@gmail.com>
2016-05-23 20:19:31 +0200

    new file  check7 changed

--- jeena_study/Mistbase/Codes_SRS/22May/check7.c
+++ jeena_study/Mistbase/Codes_SRS/22May/check7.c
@@ -509,6 +509,28 @@ uint32_t Get_nb(uint32_t HoppingBandwidth, uint32_t B, uint32_t n_ul_rb, uint32_
     return n_b;
 }
 
+/***********************************************************************************************/
+uint32_t Get_K_Tc_p(uint32_t Cyclic_shift, uint32_t N_Tx, uint32_t K_Tc)
+{
+    uint32_t K_Tc_bar = K_Tc;
+    int p_index;
+    uint32_t K_Tc_p;//K_Tc_p={0,1,...SRS_UL.K_Tc-1}
+    static const unsigned n_SRS_cs = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+    static const unsigned p_hat = (1 << 1) | (1 << 3) ;
+    /*find K_Tc_p*/
+    for (p_index = 0; p_index <=3; p_index++)
+    {
+        if (((1 << Cyclic_shift) & n_SRS_cs) && ((1 << p_index) & p_hat) && (N_Tx == 4) ) // p_hat ands p relation table Table 5.2.1-1
+        {
+            K_Tc_p = 1 - K_Tc_bar;// for p_index = 1,3
+        }
+        else 
+        {
+            K_Tc_p = K_Tc_bar;// for p_index = 0,2
+        }
+    }
+    return K_Tc_p;
+}
 
 void main()
 {
