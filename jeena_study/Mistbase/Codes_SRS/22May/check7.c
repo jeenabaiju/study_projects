4e780195bee05209717512b8cde27d797152c1f6
jeebai <jeensjohn@gmail.com>
2016-05-23 21:57:52 +0200

    new file  check7 changed

--- jeena_study/Mistbase/Codes_SRS/22May/check7.c
+++ jeena_study/Mistbase/Codes_SRS/22May/check7.c
@@ -542,6 +542,7 @@ uint32_t get_k_0_pbar(uint32_t bw_cfg, uint32_t N_sc, uint32_t n_ul_rb ,uint32_t
     if (bw_cfg < 8)
     {
 		  K_0_temp = (floor(n_ul_rb / 2)) -  (m_srs_b[srsbwtable_idx(n_ul_rb)][0][bw_cfg] / 2 );
+		  printf("k_0_temp= %d\n",K_0_temp );
           k_0_pbar =(K_0_temp *N_sc ) + K_Tc_p;
 		  //k_0_pbar = (((floor(n_ul_rb / 2)) - (m_srs_0 / 2 ))*N_sc ) + K_Tc_p;
 		  printf("k_0_pbar 1= %d\n",k_0_pbar);
