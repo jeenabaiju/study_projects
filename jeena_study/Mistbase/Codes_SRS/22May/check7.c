b105d1751460ce934ed3ac200f4854deca626b80
jeebai <jeensjohn@gmail.com>
2016-05-23 22:25:42 +0200

    new file  check7 changed

--- jeena_study/Mistbase/Codes_SRS/22May/check7.c
+++ jeena_study/Mistbase/Codes_SRS/22May/check7.c
@@ -642,7 +642,7 @@ void main()
 		uint32_t k_0_pbar = get_k_0_pbar( bw_cfg,  N_sc,  n_ul_rb , K_Tc_p,m_srs_0);
 		printf("k_0_pbar = %d\n",k_0_pbar);
 		uint32_t  M_sc = Get_Msc_values(bw_cfg, B,  n_ul_rb, N_sc);
-		Get_Freq_domain_start_k0p(uB,   bw_cfg, K_Tc,  M_sc,  k_0_pbar, n_b);
+		uint32_t k_p = Get_Freq_domain_start_k0p(B,   bw_cfg, K_Tc,  M_sc,  k_0_pbar, n_b);
 		
 		
 }
