0fa5278d2e16e98b9437e3c9db2bd68716d68955
jeebai <jeensjohn@gmail.com>
2016-06-05 23:24:39 +0200

    changed June2 main.c file

--- jeena_study/Mistbase/Codes_SRS/Pucch/June2/main.c
+++ jeena_study/Mistbase/Codes_SRS/Pucch/June2/main.c
@@ -943,6 +943,7 @@ int get_n_cs_cell(struct pucch_config *cfg, uint32_t n_cs_cell[cfg->NSLOTS_X_FRA
     uint8_t n_prs[len];
 	const uint16_t N_ID = Get_cellID(srs_ul);
 	const uint32_t c_init = N_ID;
+	printf("NID = %d \n",N_ID);
 	calc_prs_c( c_init, len, n_prs);
     uint32_t n_cs_cell1[2][3] = {{192, 46, 212},
                       {91, 84, 25}};
