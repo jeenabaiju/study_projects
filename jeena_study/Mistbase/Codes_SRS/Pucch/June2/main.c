cb5063209b5f66761fdd695b89564a6ef2122d93
jeebai <jeensjohn@gmail.com>
2016-06-06 23:08:45 +0200

    changed June2 main.c file

--- jeena_study/Mistbase/Codes_SRS/Pucch/June2/main.c
+++ jeena_study/Mistbase/Codes_SRS/Pucch/June2/main.c
@@ -1133,11 +1133,12 @@ uint32_t get_pucch_format1(struct pucch_config *cfg,uint32_t ns, uint32_t l,stru
 	         /****************************************************************************/
 	// ALPHA PUCCH format1/1a/1b
 	/****************************************************************************/
-    float alpha[cfg->NSLOTS_X_FRAME][c];
+
     *n_oc = ((ns % 2)== 0)?n_oc1[nslot]:n_oc1[nslot+1];
     printf("n_oc1 = %d \nn_oc2= %d\n",n_oc1[nslot],n_oc1[nslot+1]);
         }
         }
+        float alpha[cfg->NSLOTS_X_FRAME][c];
     for (nslot = 0; nslot < cfg->NSLOTS_X_FRAME; nslot++)
 	  {
 	    for (l = 0; l < CP_NSYMB ; l++)
