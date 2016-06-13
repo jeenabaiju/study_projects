cb107970dea0ea14a89241eb52b68db4d63fb19e
jeebai <jeensjohn@gmail.com>
2016-06-13 19:22:29 +0200

    new file cahnged ref_ul

--- jeena_study/Mistbase/PUCCH/ref_ul_pucch_srsv2.c
+++ jeena_study/Mistbase/PUCCH/ref_ul_pucch_srsv2.c
@@ -1935,10 +1935,12 @@ uint32_t get_pucch_mvalue(struct pucch_config *cfg, uint32_t format, uint32_t n_
     	  m = n_pucch / N_SF0_PUCCH;
     	  break;
      case format_4:
+         unt32_t m1[cfg->M_RB_pucch4];
          for( i =0;i < cfg->M_RB_pucch4; i++)
          {
-            m[i] = cfg->n_pucch_4 + i;
+            m1[i] = cfg->n_pucch_4 + i;
          }
+            m = m1[i];
          break;
      case format_5:
     	  m = n_pucch;
