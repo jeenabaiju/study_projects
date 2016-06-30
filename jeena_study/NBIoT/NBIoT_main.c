bab599dc60674f2fb2bebf51603076b3ff4ff4b0
jeebai <jeensjohn@gmail.com>
2016-06-30 21:32:46 +0200

    changed NBIotmain

--- jeena_study/NBIoT/NBIoT_main.c
+++ jeena_study/NBIoT/NBIoT_main.c
@@ -750,7 +750,7 @@ int refsignal_dmrs_npusch_map(struct NPUSCH *npusch, uint32_t loc, float complex
                                    for (nslots = 0; nslots<Nslots;nslots++)
                                    {
 		                               Symbol[k][loc[l]+nslots*CP_NSYMB] =  r_npusch[k] ;
-		                                printf(" Symbol[%d][%d] = %.4f+i%.4f \n",k,loc[l]+nslots*CP_NSYMB,creal(Symbol[k][loc[l]+nslots*CP_NSYMB]),cimag(Symbol[k][loc[l]+nslots*CP_NSYMB]));
+		                                printf(" Symbol[%d][%d] = %.4f+i%.4f \n",k,loc[l],creal(Symbol[k][loc[l]]),cimag(Symbol[k][loc[l]]));
 		                              // Mapping_slots = repmat(Symbol,1,M_identical-1);
 		     					   }
                                   }
